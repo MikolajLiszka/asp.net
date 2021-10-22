3bad037921542416a7887778b151f4866512f0be
MikolajLiszka <mikolaj.liszka@gmail.com>
2021-10-22 13:00:31 +0200

    a
    a

---
+++ wypozyczalnia_gier/wypozyczalnia_gier/Controllers/GryController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using wypozyczalnia_gier.Models;
+
+namespace wypozyczalnia_gier.Controllers
+{
+    public class GryController : Controller
+    {
+
+        static List<Gra> gryLista = new List<Gra>()
+        {
+            new Gra(){TytulGry = "Diablo III", KategoriaGry = "rpg", PoziomTrudnosciGry = "średnia", DataStworzeniaGry = DateTime.Parse("2008-10-10"), CenaWypozyczeniaGry = "10 złotych"}
+        };
+        static int counter = 0;
+
+        public IActionResult ListaGier()
+        {
+            return View(gryLista);
+        }
+
+        public IActionResult Edit(int Id)
+        {
+            Gra gra = gryLista.Find(s => s.Id == Id);
+            foreach(var game in gryLista)
+            {
+                if(game.Id == Id)
+                {
+                    gra = game;
+                    break;
+                }
+            }
+            return View("EditForm", gra);
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Add()
+        {
+            return View("AddForm");
+        }
+        [HttpPost]
+        public IActionResult Add(Gra gra)
+        {
+            if (ModelState.IsValid)
+            {
+                gra.Id = counter++;
+                gryLista.Add(gra);
+                return View("GraDodana", gryLista);
+            }
+            else
+            {
+                return View("AddForm");
+            }
+        }
+    }
+}
---
+++ wypozyczalnia_gier/wypozyczalnia_gier/Models/Gra.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace wypozyczalnia_gier.Models
+{
+    public class Gra
+    {
+        [HiddenInput]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Wpisz tytuł gry!")]
+        public string TytulGry { get; set; }
+        
+        [Required(ErrorMessage = "Wpisz kategorię gry!")]
+        public string KategoriaGry { get; set; }
+        
+        [Required(ErrorMessage = "Wpisz poprawną datę!")]
+        public DateTime DataStworzeniaGry { get; set; }
+
+        [Required(ErrorMessage = "Wpisz poziom trudności gry!")]
+        public string PoziomTrudnosciGry { get; set; }
+
+        [Required(ErrorMessage = "Wpisz cenę wypożyczenia gry!")]
+        public string CenaWypozyczeniaGry { get; set; }
+    }
+}
