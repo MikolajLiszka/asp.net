d532ffb3a76c2906279b9d229981130e53457bcc
MikolajLiszka <mikolaj.liszka@gmail.com>
2022-01-27 19:48:57 +0100

    testy prawie

--- wypozyczalnia_gier/wypozyczalniaGierTest/ApiControllerTests.cs
+++ wypozyczalnia_gier/wypozyczalniaGierTest/ApiControllerTests.cs
@@ -2,13 +2,14 @@
 using wypozyczalnia_gier.Models;
 using Xunit;
 using System.Linq;
+using Microsoft.AspNetCore.Mvc;
 
 namespace wypozyczalniaGierTest
 {
     public class ApiControllerTests
     {
         [Fact]
-        public void GetBooksTest()
+        public void GetGrasTest()
         {
             var repository = new MemoryGryRepository();
             repository.Add(new Gra() { TytulGry = "TEST" });
@@ -16,6 +17,34 @@ namespace wypozyczalniaGierTest
             var controller = new ApiController(repository);
             var games = controller.GetGras();
             Assert.Equal(2, games.Count());
+        }
+
+        /*
+        [Fact]
+        public void DeleteGraTest()
+        {
+            var repository = new MemoryGryRepository();
+            repository.Add(new Gra() { TytulGry = "TEST" });
+            repository.Add(new Gra() { TytulGry = "TEST" });
+            var controller = new ApiController(repository);
+            Microsoft.AspNetCore.Mvc.IActionResult actionResult = controller.DeleteGra(2);
+            Assert.IsType<OkResult>(actionResult);
+            actionResult = controller.DeleteGra(6);
+            Assert.IsType<NotFoundResult>(actionResult);
+        }
+        */
+
+        [Fact]
+        public void AddGraTest()
+        {
+            var repository = new MemoryGryRepository();
+            repository.Add(new Gra() { TytulGry = "TEST" });
+            repository.Add(new Gra() { TytulGry = "TEST" });
+            ApiController controller = new ApiController(repository);
+            Gra newGra = new Gra() { Id = 10, TytulGry = "NEW" };
+            ActionResult<Gra> actionResult = controller.AddGra(newGra);
+            Assert.Equal("NEW", repository.Find(3).TytulGry);
+
         }
     }
 }
